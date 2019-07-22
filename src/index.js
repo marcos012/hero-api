2976cf08813c9c24fff216f433d9218300a6b6aa
marcos012 <marcospaulo.ad@gmail.com>
2019-07-22 01:22:01 -0300

    Criado metodos de update e delete + Fix cors

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
+const cors = require('cors');
 require('dotenv/config');
 
+app.use(cors());
 app.use(bodyParser.json());
 
 const heroesRoute = require('./routes/heroes');
@@ -11,7 +13,7 @@ const heroesRoute = require('./routes/heroes');
 app.use('/heroes', heroesRoute);
 
 app.get('/', (req, res) => {
-    res.send('ola');
+    res.send('Hero API');
 });
 
 mongoose.connect(process.env.DB_CONNECTION,
--- src/routes/heroes.js
+++ src/routes/heroes.js
@@ -21,4 +21,38 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.get('/:heroId', async (req, res) => {
+    try {
+        const hero = await Hero.findById(req.params.heroId);
+        res.json(hero);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+router.delete('/:heroId', async (req, res) => {
+    try {
+        const removedHero = await Hero.remove({ _id: req.params.heroId });
+        res.json(removedHero);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+router.patch('/:heroId', async (req, res) => {
+    try {
+        const updatedPost = await Hero.updateOne(
+            { _id: req.params.heroId },
+            {
+                $set: {
+                    name: req.body.name,
+                    age: req.body.age,
+                    power: req.body.power
+                }
+            });
+        res.json(updatedPost);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
 module.exports = router;
