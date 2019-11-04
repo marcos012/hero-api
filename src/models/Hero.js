03eaeb1e6524ff629dee55d4b3266e29310b54cb
marcos012 <marcospaulo.ad@gmail.com>
2019-11-04 03:32:26 -0200

    Refatorado objeto Hero

--- src/models/Hero.js
+++ src/models/Hero.js
@@ -1,21 +1,26 @@
 const mongoose = require('mongoose');
 
 const HeroSchema = new mongoose.Schema({
+    id: {
+        type: Number, unique: true, required: true
+    },
     name: {
-        type: String,
-        required: true
+        type: String, required: true
     },
-    age: {
-        type: Number,
-        required: true,
+    slug: {
+        type: String, required: true
     },
-    power: {
-        type: String,
-        required: true,
+    powerstats: {
+        type: Object, required: true
+    },
+    biography: {
+        type: Object, required: true
+    },
+    images: {
+        type: Object, required: true
     },
     createdAt: {
-        type: Date,
-        default: Date.now
+        type: Date, default: Date.now
     }
 });
 
